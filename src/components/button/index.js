7223166ce92e814b7917eb1b56db2ffc7d34dcb2
Josué Cardoso <josuecardoso_dosul@hotmail.com>
2020-06-13 19:18:48 -0300

    Adicionado evento de onClick ao button

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 import { Button } from './styles'
 
-function button({value}) {
+function CustomButton({value}) {
+    const history = useHistory();
+
+    const onClickEvent = () => {
+
+        if(value === "Entrar"){
+            //Fazer as validações, autenticações e alterar de página
+            let path = `/chat`; 
+            history.push(path);
+        }
+
+        if(value === "Registrar"){
+            //Fazer as validações, enviar o email e limpar o formulário
+        }
+
+    }
+
     return (
-        <Button>{value}</Button>
+        <Button onClick={onClickEvent}>{value}</Button>
     )
 }
 
-export default button;
+export default CustomButton;
