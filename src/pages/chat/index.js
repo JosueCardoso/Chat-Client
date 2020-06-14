387eb79d25978999dd3681d0652bd425dd455b35
Josué Cardoso <josuecardoso_dosul@hotmail.com>
2020-06-14 17:16:20 -0300

    Alterado as classes para serem funções nas páginas

--- src/pages/chat/index.js
+++ src/pages/chat/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import { Container, MainBox, ChatContainer } from './styles'
 
@@ -6,18 +6,18 @@ import ProjectTitle from '../../components/projectTitle'
 import ChatComponent from '../../components/chat'
 import UsersOnline from '../../components/usersOnline'
 
-export default class Chat extends Component {
-    render() {
-        return (
-            <Container>
-                <ProjectTitle/>
-                <MainBox>
-                    <ChatContainer>                        
-                        <ChatComponent/>
-                        <UsersOnline/>
-                    </ChatContainer>
-                </MainBox>
-            </Container>
-        )
-    }
+function Chat({socketIOClient}) {
+ return (
+    <Container>
+        <ProjectTitle/>
+        <MainBox>
+            <ChatContainer>                        
+                <ChatComponent/>
+                <UsersOnline/>
+            </ChatContainer>
+        </MainBox>
+    </Container>
+)
 }
+
+export default Chat;
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react'
-import SocketIOClient from "socket.io-client";
+import React, { useEffect } from 'react'
 
 import ProjectTitle from '../../components/projectTitle'
 import Input from '../../components/input'
@@ -14,20 +13,10 @@ import {
         Title
         } from './styles'
 
-export default class Login extends Component {  
-
-  handleSocketIO = () => {
-    const socket = SocketIOClient("http://127.0.0.1:4001"); 
+function Login({socketIOClient}) {
+  const handleSocketIO = () => {
+    const socket = socketIOClient;
     
-      let tries = 0;
-      //Caso o servidor esteja offline, tentará se conectar até três vezes
-      socket.on('reconnecting', () => {     
-        tries++; 
-        if (tries === 3) {
-             socket.disconnect();
-        }    
-      });
-
       //Recebe a resposta do servidor
       socket.on('responseStatus', (response) => {
           if(response === "USER_AUTHENTICATED"){
@@ -45,33 +34,33 @@ export default class Login extends Component {
       });
   }
 
-  componentDidMount(){
-    this.handleSocketIO();
-  }
+  useEffect(() => {
+    handleSocketIO();
+  });
 
-  render() {  
-    return (
-      <Container>
-        <ProjectTitle/>
-        <MainBox>
-          <LoginContainer>
-            <Title>Login</Title>
-            <Input placeholder="Usuário"/>
-            <Input type="password" placeholder="Senha"/>
-            <Button value="Entrar"/>
-          </LoginContainer>
+  return (
+    <Container>
+      <ProjectTitle/>
+      <MainBox>
+        <LoginContainer>
+          <Title>Login</Title>
+          <Input placeholder="Usuário"/>
+          <Input type="password" placeholder="Senha"/>
+          <Button value="Entrar"/>
+        </LoginContainer>
 
-          <Divider/>
+        <Divider/>
 
-          <RegisterContainer>
-            <Title>Cadastro</Title>
-            <Input placeholder="Usuário"/>
-            <Input type="password" placeholder="Senha"/>
-            <Input placeholder="Email"/>
-            <Button value="Registrar"/>
-          </RegisterContainer>
-        </MainBox>        
-      </Container>
-    )
-  }
+        <RegisterContainer>
+          <Title>Cadastro</Title>
+          <Input placeholder="Usuário"/>
+          <Input type="password" placeholder="Senha"/>
+          <Input placeholder="Email"/>
+          <Button value="Registrar"/>
+        </RegisterContainer>
+      </MainBox>        
+    </Container>
+  )
 }
+
+export default Login;
