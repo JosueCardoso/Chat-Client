f1b5ff236fecb9f71ad8abdfb964c240db2b4f71
Josué Cardoso <josuecardoso_dosul@hotmail.com>
2020-06-14 17:32:57 -0300

    Adicionado tratamento para rotas privadas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
-import { BrowserRouter, Switch, Route  } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Redirect  } from 'react-router-dom'
 import SocketIOClient from "socket.io-client";
 
 import App from '../pages/login'
 import Chat from '../pages/chat'
 import Page404 from '../pages/page404'
 
+const isAuthenticated = () => false; //TODO: Alterar para consultar no banco
 const socketIOClient = SocketIOClient("http://127.0.0.1:4001");
 
 const reconnectingEvent = () => {
@@ -20,6 +21,19 @@ const reconnectingEvent = () => {
   });
 }
 
+const PrivateRoute = ({component: Component, ...rest}) => (
+  <Route
+    {...rest} 
+    render={props =>
+      isAuthenticated() ? (
+        <Component {...props}/>
+      ) : (
+        <Redirect to={{pathname: "/", state: {from: props.location}}}/>
+      )
+    }
+  />
+)
+
 const Routes = () => {
   reconnectingEvent();
 
@@ -27,7 +41,7 @@ const Routes = () => {
     <BrowserRouter>
         <Switch>
           <Route path="/" exact={true} component={() => (<App socketIOClient = {socketIOClient}/>)}/>
-          <Route path="/chat" component={() => (<Chat socketIOClient = {socketIOClient}/>)}/>
+          <PrivateRoute path="/chat" component={() => (<Chat socketIOClient = {socketIOClient}/>)}/>
           <Route path="*" component={Page404}/>
         </Switch>          
     </BrowserRouter>
